f1df80fc0af7928a0fdb9b7ced8d640fd0013628
hiroshiuk1991 <hiroshis91@icloud.com>
2020-03-21 17:14:26 +0000

    icons added

--- src/components/day.js
+++ src/components/day.js
@@ -1,5 +1,6 @@
 import React from 'react'
-var moment = require('moment');
+import WeatherIcon from 'react-icons-weather';
+var moment = require('moment')
 
 export default function Day (props) {
   let newDate = new Date()
@@ -9,18 +10,21 @@ export default function Day (props) {
   const imgURL = `owf owf-${props.reading.weather[0].id} owf-5x`
 
   return (
-      <div className="col-sm-3">
-          <div className="card">
-              <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
-              <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
-              <i className={imgURL}></i>
-              <h2>{Math.round(props.reading.main.temp)} °C</h2>
-              <h4>Min:{Math.round(props.reading.main.temp_min)} °C</h4>
-              <h4>Max:{Math.round(props.reading.main.temp_max)} °C</h4>
-              <div className="card-body">
-                  <p className="card-text">{props.reading.weather[0].description}</p>
-              </div>
-          </div>
+    <div className='col-sm-4'>
+      <div className='card'>
+        <h3 className='card-title'>{moment(newDate).format('dddd')}</h3>
+        <p className='text-muted'>
+          {moment(newDate).format('MMMM Do, h:mm a')}
+        </p>
+        <WeatherIcon name='owm' iconId={props.reading.weather[0].id} />
+        {/* <i className={imgURL}></i> */}
+        <h1>{Math.round(props.reading.main.temp)} °C</h1>
+        <h5>Min:{Math.round(props.reading.main.temp_min)} °C</h5>
+        <h5>Max:{Math.round(props.reading.main.temp_max)} °C</h5>
+        <div className='card-body'>
+          <h2 className='card-text'>{props.reading.weather[0].description}</h2>
+        </div>
       </div>
+    </div>
   )
 }
